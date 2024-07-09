ed4550e3943f169f4f4263ae25eccc82edd4ecdb
Nympha Pamba <pambapraise@gmail.com>
2024-07-09 12:57:02 +0300

    adds .results.json; index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newCustomer) {
+  bestCustomer = newCustomer;
+}
+
+
+const leastFavoriteCustomer = 'some initial value';
+
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value'; 
+}
